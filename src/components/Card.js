b8955bad863dcf0b03fef983b6b300e8efd306e7
Zeta7 <edson.joan.m.o97@gmail.com>
2022-03-28 21:34:21 -0500

    validacion de condicional ===

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,24 +14,24 @@ const Card = ({url}) =>{
         .then(res=>{
             setpokemon(res.data)
         });
-        if(pokemon.types?.[0].type.name == "grass"){setColor("#50df9c")};
-        if(pokemon.types?.[0].type.name == "fire"){setColor("#D93F3F")};
-        if(pokemon.types?.[0].type.name == "water"){setColor("#83B9FF")};
-        if(pokemon.types?.[0].type.name == "bug"){setColor("#36f04b")};
-        if(pokemon.types?.[0].type.name == "normal"){setColor("#6d2424")};
-        if(pokemon.types?.[0].type.name == "poison"){setColor("#600c64")};
-        if(pokemon.types?.[0].type.name == "electric"){setColor("#ffe944")};
-        if(pokemon.types?.[0].type.name == "ground"){setColor("#ffcc00")};
-        if(pokemon.types?.[0].type.name == "fairy"){setColor("#dd3360")};
-        if(pokemon.types?.[0].type.name == "dark"){setColor("#30015f")};
-        if(pokemon.types?.[0].type.name == "fighting"){setColor("#b33008")};
-        if(pokemon.types?.[0].type.name == "psychic"){setColor("#B1DBBC")};
-        if(pokemon.types?.[0].type.name == "rock"){setColor("#414141")};
-        if(pokemon.types?.[0].type.name == "ghost"){setColor("#3212c0")};
-        if(pokemon.types?.[0].type.name == "ice"){setColor("#00e5ff")};
-        if(pokemon.types?.[0].type.name == "dragon"){setColor("#02cc64")};
-        if(pokemon.types?.[0].type.name == "steel"){setColor("#c7073a")};
-        if(pokemon.types?.[0].type.name == "flying"){setColor("#237e04")};
+        if(pokemon.types?.[0].type.name === "grass"){setColor("#50df9c")};
+        if(pokemon.types?.[0].type.name === "fire"){setColor("#D93F3F")};
+        if(pokemon.types?.[0].type.name === "water"){setColor("#83B9FF")};
+        if(pokemon.types?.[0].type.name === "bug"){setColor("#36f04b")};
+        if(pokemon.types?.[0].type.name === "normal"){setColor("#6d2424")};
+        if(pokemon.types?.[0].type.name === "poison"){setColor("#600c64")};
+        if(pokemon.types?.[0].type.name === "electric"){setColor("#ffe944")};
+        if(pokemon.types?.[0].type.name === "ground"){setColor("#ffcc00")};
+        if(pokemon.types?.[0].type.name === "fairy"){setColor("#dd3360")};
+        if(pokemon.types?.[0].type.name === "dark"){setColor("#30015f")};
+        if(pokemon.types?.[0].type.name === "fighting"){setColor("#b33008")};
+        if(pokemon.types?.[0].type.name === "psychic"){setColor("#B1DBBC")};
+        if(pokemon.types?.[0].type.name === "rock"){setColor("#414141")};
+        if(pokemon.types?.[0].type.name === "ghost"){setColor("#3212c0")};
+        if(pokemon.types?.[0].type.name === "ice"){setColor("#00e5ff")};
+        if(pokemon.types?.[0].type.name === "dragon"){setColor("#02cc64")};
+        if(pokemon.types?.[0].type.name === "steel"){setColor("#c7073a")};
+        if(pokemon.types?.[0].type.name === "flying"){setColor("#237e04")};
     },[url, pokemon]);
 
     
@@ -40,7 +40,7 @@ const Card = ({url}) =>{
     return(
 
             <div className='cont-card-pokemon' style={{background: color, border: `8px solid ${color}`}}>
-                <Link to={`/pokedex/${pokemon.id}`} className="card-poke">
+                <Link to={`/pokedex/${pokemon.id}` } className="card-poke">
                     <div className="c0">
                         <img src={pokemon.sprites?.other.dream_world.front_default} />
                     </div>
